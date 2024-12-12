baad840f71ed3be0279101ed3582cd59abccf036
Pauline Umunyana <pauline.umunyana@moyotech.solutions>
2024-12-12 15:05:17 +0200

    adding employee details

--- webapp/controller/EmployeeDetail.controller.js
+++ webapp/controller/EmployeeDetail.controller.js
@@ -1,63 +1,47 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",  // Import the JSONModel
+    "sap/m/MessageToast"            // Import MessageToast for user notifications
+], function (Controller, UIComponent, JSONModel, MessageToast) {
     "use strict";
 
     return Controller.extend("com.myorg.myapp.controller.EmployeeDetail", {
 
         onInit: function () {
-            // Get the product ID from the route
-            var oRouter = this.getOwnerComponent().getRouter();
-            oRouter.getRoute("employee").attachPatternMatched(this._onRouteMatched, this);
-          },
+            // Get the router correctly using UIComponent.getRouterFor(this)
+            var oRouter = UIComponent.getRouterFor(this);
 
-          _onRouteMatched: function (oEvent) {
-            var sProductId = oEvent.getParameter("arguments").EmployeeID;
-            
-            // Set the binding path to the product details based on the ID
-            var oModel = this.getView().getModel();
-            var oProduct = oModel.getProperty("/Employees").find(function (product) {
-              return product.id === sProductId;
-            });
-            
-            if (oProduct) {
-              this.getView().bindObject({
-                path: "/" + this.getProductIndex(sProductId),
-                model: "Employees"
-              });
-            }
-          },
-          getProductIndex: function (sProductId) {
-            var oModel = this.getView().getModel();
-            var aProducts = oModel.getProperty("/Employees");
-            return aProducts.findIndex(function (product) {
-              return product.id === sProductId;
-            });
-          },
-          onShowResume:function(){
-            var oCtx = this.getView().getBindingContext();
+            // Attach the matched event for the "employeeDetail" route
+            oRouter.getRoute("employeeDetail").attachMatched(this._onRouteMatched, this);
+        },
 
-			this.getRouter().navTo("employeeResume", {
-				employeeId : oCtx.getProperty("EmployeeID")
-			});
-          }
+        _onRouteMatched: function (oEvent) {
+            var oArgs = oEvent.getParameter("arguments");
+            var sEmployeeId = oArgs.EmployeeID; // Assuming employeeId is passed in the route
 
-		// _onBindingChange : function (oEvent) {
-		// 	// No data for the binding
-		// 	if (!this.getView().getBindingContext()) {
-		// 		this.getRouter().getTargets().display("notFound");
-		// 	}
-		// },
+            // Log the employeeId (for debugging)
+            console.log("Employee ID: " + sEmployeeId);
 
-		// onShowResume : function (oEvent) {
-		// 	var oCtx = this.getView().getBindingContext();
+            // Fetch employee data from the JSON model
+            var oModel = new JSONModel("model/Employees.json");
+            this.getView().setModel(oModel, "employee");
 
-		// 	this.getRouter().navTo("employeeResume", {
-		// 		employeeId : oCtx.getProperty("EmployeeID")
-		// 	});
-		// }
+            // After loading the model, filter the data to show the specific employee's details
+            oModel.attachRequestCompleted(function () {
+                var oData = oModel.getData();
+                var oEmployee = oData.Employees.find(function (employee) {
+                    return employee.EmployeeID === parseInt(sEmployeeId, 10); // Match the EmployeeID
+                });
 
-	});
-
+                if (oEmployee) {
+                    // Set the model with the filtered employee data
+                    this.getView().setModel(new JSONModel(oEmployee), "employee");
+                    console.log("Employee Details:", oEmployee); // For debugging
+                } else {
+                    MessageToast.show("Employee not found");
+                }
+            }, this);
+        }
+    });
 });
